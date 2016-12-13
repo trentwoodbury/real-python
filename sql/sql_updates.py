7d48ef97cd792de2edfc1ceb32dd746ded6c7f3e
Trent Woodbury <trentwoodbury@gmail.com>
2016-12-13 11:31:47 -0700

    Add sql_updates.py

---
+++ sql/sql_updates.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+with sqlite3.connect("new.db") as conn:
+    cursor = conn.cursor()
+    cursor.execute("""
+    INSERT INTO population
+    VALUES('New York City', 'NY', 8200000)
+    """)
+
+    cursor.execute("""
+    INSERT INTO population
+    VALUES('San Francisco', 'CA', 800000)
+    """)
+
+    cursor.execute("""
+    INSERT INTO population
+    VALUES('Denver', 'CO', 800000)
+    """)
