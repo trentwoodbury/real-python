790049bb621742f6b82829282157be9d4c9bd3ef
Trent Woodbury <trentwoodbury@gmail.com>
2016-12-13 12:24:02 -0700

    Bug fixes. Comment out add table

--- sql/sql_example/assignment3a.py
+++ sql/sql_example/assignment3a.py
@@ -1,16 +1,17 @@
-import sqllite3
+import sqlite3
 import numpy as np
 
 rand_nums = np.random.choice(100, 100)
 
 with sqlite3.connect("newnum.db") as conn:
     c = conn.cursor()
-    c.execute("""
-    CREATE TABLE numbers
-    (num INT);
-    """)
+    # c.execute("""
+    # CREATE TABLE numbers
+    # (num INT);
+    # """)
 
-    c.exectuemany("""
-    INSERT INTO numbers(num)
-    VALUES (?)
-    """, rand_nums)
+    for i in range(100):
+        c.execute("""
+        INSERT INTO numbers(num)
+        VALUES (?)
+        """, (rand_nums[i], ))
